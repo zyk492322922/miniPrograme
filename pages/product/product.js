d6ccf3cd75fd44ac6baa9c853b715a9ffd9eed99
zhangyunkai <zhangyunkai@hyjf.com>
2019-05-04 19:33:03 +0800

    add utils

--- pages/product/product.js
+++ pages/product/product.js
@@ -4,10 +4,9 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '',
+    imageList: [],
     userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    hasUserInfo: false
   },
   //事件处理函数
   bindViewTap: function() {
@@ -21,43 +20,24 @@ Page({
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true,
-        motto: "查看我的"
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback1 = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true,
-          motto: "查看我的"
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true,
-            motto: "查看我的"
-          })
-        }
+        imageList: []
       })
     }
   },
-  // 获取用户信息方法
-  getUserInfo: function(e) {
-    console.log("cccc " + e)
-    // 判断是否获取授权，否则不设置用户信息和跳转链接
-    if(e.detail.userInfo){
-      app.globalData.userInfo = e.detail.userInfo
-      this.setData({
-        userInfo: e.detail.userInfo,
-        hasUserInfo: true,
-        motto: "查看我的"
-      })
-    }
+
+  // 上传图片控件事件
+  chooseImage(e){
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],  // 原图和压缩后的图
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        console.log(res.tempFilePaths)
+        const imageSet = this.data.imageList.concat(res.tempFilePaths)
+        // 自定义设置限制只能上传3张图片
+        this.data.imageList = imageSet.length <= 3 ? imageSet : imageSet.slice(0, 3)
+        $digest(this)
+      }
+    })
   }
+ 
 })
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,27 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const $init = (page) => {
+  page.$data = objectUtil.$copy(page.data, true)
+}
+
+const $digest = (page) => {
+  let data = page.data
+  let $data = page.$data
+  let ready2set = {}
+
+  for (let k in data) {
+    if (!objectUtil.$isEqual(data[k], $data[k])) {
+      ready2set[k] = data[k]
+      $data[k] = objectUtil.$copy(data[k], true)
+    }
+  }
+
+  if (Object.keys(ready2set).length) {
+    page.setData(ready2set)
+  }
+}
+
 module.exports = {
   formatTime: formatTime
 }
